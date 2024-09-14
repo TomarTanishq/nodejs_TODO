9bba9fb547c40bbb3b9067da3c474ca54cb0f4a8
TomarTanishq <tanishqtomar6969@gmail.com>
2024-09-14 22:43:07 +0530

    fixed engine

--- routes/task.js
+++ routes/task.js
@@ -2,11 +2,17 @@ import { deleteTask, newTask, updateTask } from "../controllers/task.js"
 import express from "express"
 import { isAuthenticated } from "../middlewares/auth.js"
 import cookieParser from "cookie-parser"
+import cors from "cors"
 import { showTask } from "../controllers/task.js"
 
 const router = express.Router()
 
 router.use(cookieParser())
+router.use(cors({
+    origin:[process.env.FRONTEND_URL],
+    methods:["GET","POST","PUT","DELETE"],
+    credentials:true
+}))
 
 router.post("/new", isAuthenticated, newTask)
 
